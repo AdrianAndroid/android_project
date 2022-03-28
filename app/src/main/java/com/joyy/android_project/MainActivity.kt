9c42291f4f6cf3e39a4afbe3041c0403fa02ce03
zhaojian2 <zhaojian2@joyy.com>
2022-03-28 15:40:17 +0800

    first-commit

---
+++ app/src/main/java/com/joyy/android_project/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.joyy.android_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Dispatch onResume() to fragments.
+     */
+    override fun onPostResume() {
+        super.onPostResume()
+    }
+
+    /**
+     * This is the fragment-orientated version of {@link #onResume()} that you
+     * can override to perform operations in the Activity at the same point
+     * where its fragments are resumed.  Be sure to always call through to
+     * the super-class.
+     */
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+    }
+}
