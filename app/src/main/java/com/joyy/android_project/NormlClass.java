67027c9447e44db86dcc20f7bcba5e4aa6e8e852
zhaojian2 <zhaojian2@joyy.sg>
2022-04-07 12:05:53 +0800

    Handler

---
+++ app/src/main/java/com/joyy/android_project/NormlClass.java
@@ -0,0 +1,27 @@
+package com.joyy.android_project;
+
+import android.os.Handler;
+import androidx.annotation.NonNull;
+
+/**
+ * Time:2022/4/7 11:53
+ * Author:
+ * Description:
+ */
+import android.os.Message;
+
+import java.util.logging.LogRecord;
+
+public class NormlClass {
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            send();
+        }
+    } ;
+
+    void send() {
+        handler.sendEmptyMessage(0);
+    }
+}
