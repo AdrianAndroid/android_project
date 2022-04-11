47bbe826cfed535ca1738b8ba12a540e737c23f2
zhaojian2 <zhaojian2@joyy.sg>
2022-04-11 12:18:28 +0800

    写了一个demo

---
+++ app/src/main/java/com/joyy/android_project/BlankFragment.java
@@ -0,0 +1,105 @@
+package com.joyy.android_project;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BlankFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BlankFragment extends Fragment {
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment BlankFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static BlankFragment newInstance() {
+        BlankFragment fragment = new BlankFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        MainActivity.Companion.log("onAttach", "Fragment");
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        MainActivity.Companion.log("onCreate", "Fragment");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        MainActivity.Companion.log("onCreateView", "Fragment");
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        MainActivity.Companion.log("onViewCreated", "Fragment");
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        MainActivity.Companion.log("onStart", "Fragment");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        MainActivity.Companion.log("onResume", "Fragment");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        MainActivity.Companion.log("onPause", "Fragment");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        MainActivity.Companion.log("onStop", "Fragment");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        MainActivity.Companion.log("onDestroyView", "Fragment");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        MainActivity.Companion.log("onDestroy", "Fragment");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        MainActivity.Companion.log("onDetach", "Fragment");
+        super.onDetach();
+    }
+}
--- app/src/main/java/com/joyy/android_project/MainActivity.kt
+++ app/src/main/java/com/joyy/android_project/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.joyy.android_project
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = "FRAGMENT"
+        fun log(msg: String, prefix: String = "Activity") {
+            Log.i(TAG, "$prefix -> $msg")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        MainActivity.log("onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.btnFinish).setOnClickListener {
+            finish()
+        }
     }
+
+    override fun onStart() {
+        log("onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        log("onResume")
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        log("onRestart")
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        log("onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        log("onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        log("onDestroy")
+        super.onDestroy()
+    }
+
 }
