a7499cbedd0896668a4d55acbf4678d9291ae3a8
zhaojian <Flannery.zhao@kikatech.com>
2022-04-24 10:14:50 +0800

    添加了两个生命周期的方法

--- app/src/main/java/com/joyy/android_project/MainActivity.kt
+++ app/src/main/java/com/joyy/android_project/MainActivity.kt
@@ -8,4 +8,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Dispatch onResume() to fragments.
+     */
+    override fun onPostResume() {
+        super.onPostResume()
+    }
+
+    /**
+     * This is the fragment-orientated version of {@link #onResume()} that you
+     * can override to perform operations in the Activity at the same point
+     * where its fragments are resumed.  Be sure to always call through to
+     * the super-class.
+     */
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+    }
 }
