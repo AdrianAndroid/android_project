59a376ff29ee1d011fe0f461ce35d9cdc45d5b41
zhaojian <flannery.zhao@kikatech.com>
2022-05-18 18:58:36 +0800

    SmoothScroller

--- app/src/main/java/com/joyy/android_project/MainActivity.kt
+++ app/src/main/java/com/joyy/android_project/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.joyy.android_project
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.TimeInterpolator
 import android.os.Bundle
+import android.view.animation.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun test() {
+        object : TimeInterpolator {
+            override fun getInterpolation(input: Float): Float {
+                return input;
+            }
+
+        };
+        // 动画加速进行
+        AccelerateInterpolator();
+        // 快速完成动画超出再回到结果样式
+        OvershootInterpolator();
+        // 先加速再减速
+        AccelerateDecelerateInterpolator();
+        // 先退后再加速前进
+        AnticipateInterpolator();
+        // 先退后再加速前进，超出终点再回到终点
+        AnticipateOvershootInterpolator();
+        // 最后阶段弹球效果
+        BounceInterpolator();
+        // 周期运动
+        CycleInterpolator(10f);
+        // 减速
+        DecelerateInterpolator();
+        // 匀速
+        LinearInterpolator();
     }
 
     /**
