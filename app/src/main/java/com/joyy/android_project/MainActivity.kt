36f141e5ffe8755eb96057e139b6ac9b7dc4851a
zhaojian <Flannery.zhao@kikatech.com>
2022-05-22 16:39:16 +0800

    例子

--- app/src/main/java/com/joyy/android_project/MainActivity.kt
+++ app/src/main/java/com/joyy/android_project/MainActivity.kt
@@ -1,30 +1,18 @@
 package com.joyy.android_project
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.joyy.android_project.test.DemoActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.click).setOnClickListener {
+            startActivity(Intent(this, DemoActivity::class.java))
+        }
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * Dispatch onResume() to fragments.
-     */
-    override fun onPostResume() {
-        super.onPostResume()
-    }
-
-    /**
-     * This is the fragment-orientated version of {@link #onResume()} that you
-     * can override to perform operations in the Activity at the same point
-     * where its fragments are resumed.  Be sure to always call through to
-     * the super-class.
-     */
-    override fun onResumeFragments() {
-        super.onResumeFragments()
-    }
 }
---
+++ app/src/main/java/com/joyy/android_project/test/DemoActivity.java
@@ -0,0 +1,42 @@
+package com.joyy.android_project.test;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.transition.TransitionManager;
+
+import com.joyy.android_project.R;
+
+public class DemoActivity extends AppCompatActivity {
+
+    ConstraintSet set1 = new ConstraintSet();
+    ConstraintSet set2 = new ConstraintSet();
+    ConstraintLayout constraintLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+        constraintLayout = findViewById(R.id.root_layout);
+        set1.clone(constraintLayout);
+        set2.clone(this, R.layout.activity_demo2);
+
+        constraintLayout.setOnClickListener(new View.OnClickListener() {
+            boolean isTest = false;
+
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(constraintLayout);
+                if (isTest) {
+                    set1.applyTo(constraintLayout);
+                } else {
+                    set2.applyTo(constraintLayout);
+                }
+                isTest = !isTest;
+            }
+        });
+    }
+}
