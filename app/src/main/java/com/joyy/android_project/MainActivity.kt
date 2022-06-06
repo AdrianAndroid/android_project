6ee3b0eaea1b2868cadbf1ffb2f7d32cd5499220
zhaojian <flannery.zhao@kikatech.com>
2022-06-06 13:40:23 +0800

    添加glide v4.10.0

--- app/src/main/java/com/joyy/android_project/MainActivity.kt
+++ app/src/main/java/com/joyy/android_project/MainActivity.kt
@@ -1,30 +1,55 @@
 package com.joyy.android_project
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Outline
 import android.os.Bundle
+import android.view.View
+import android.view.ViewOutlineProvider
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.iv).outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View?, outline: Outline?) {
+                view?.apply {
+                    outline?.apply {
+                        outline.setRoundRect(0, 0, view.width, view.height, 10f)
+                    }
+                }
+            }
+        }
+        findViewById<ImageView>(R.id.iv).clipToOutline = true;
+
+        findViewById<ImageView>(R.id.iv).setOnClickListener {
+            var iv: ImageView = it as ImageView
+            if (iv.drawable != null) {
+
+            }
+        }
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * Dispatch onResume() to fragments.
-     */
-    override fun onPostResume() {
-        super.onPostResume()
-    }
+//    fun roundCrop(source: Bitmap): Bitmap {
+//        val result =
+//            Bitmap.createBitmap(source.width, source.height, Bitmap.Config.ARGB_8888)
+//        val canvas = Canvas(result)
+//        canvas.drawRoundRect(
+//            RectF(0f, 0f, source.width.toFloat(), source.height.toFloat()),
+//            4f,
+//            4f,
+//            Paint().apply {
+//                isAntiAlias = true
+//                shader = BitmapShader(
+//                    source,
+//                    BitmapShader.TileMode.CLAMP,
+//                    BitmapShader.TileMode.CLAMP
+//                )
+//            }
+//        )
+//        //paint.setShader(BitmapShader(source))
+//        return source
+//    }
 
-    /**
-     * This is the fragment-orientated version of {@link #onResume()} that you
-     * can override to perform operations in the Activity at the same point
-     * where its fragments are resumed.  Be sure to always call through to
-     * the super-class.
-     */
-    override fun onResumeFragments() {
-        super.onResumeFragments()
-    }
 }
