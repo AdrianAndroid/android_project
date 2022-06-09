66acbbecede7956c786b1250f450e9240c5c01fa
zhaojian <flannery.zhao@kikatech.com>
2022-06-09 22:01:49 +0800

    输入法

--- app/src/main/java/com/joyy/android_project/MainActivity.kt
+++ app/src/main/java/com/joyy/android_project/MainActivity.kt
@@ -1,30 +1,24 @@
 package com.joyy.android_project
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ServiceCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.btn).setOnClickListener {
+            var systemService: InputMethodManager =
+                getSystemService(Context.INPUT_SERVICE) as InputMethodManager
+            systemService.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS)
+
+        }
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * Dispatch onResume() to fragments.
-     */
-    override fun onPostResume() {
-        super.onPostResume()
-    }
-
-    /**
-     * This is the fragment-orientated version of {@link #onResume()} that you
-     * can override to perform operations in the Activity at the same point
-     * where its fragments are resumed.  Be sure to always call through to
-     * the super-class.
-     */
-    override fun onResumeFragments() {
-        super.onResumeFragments()
-    }
 }
