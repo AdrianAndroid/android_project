f0e034ff4a169175464b92373b27d5abd199be09
zhaojian <flannery.zhao@kikatech.com>
2022-08-27 00:17:20 +0800

    PositionGroup

--- app/src/main/java/com/joyy/android_project/MainActivity.kt
+++ app/src/main/java/com/joyy/android_project/MainActivity.kt
@@ -1,30 +1,36 @@
 package com.joyy.android_project
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.telephony.TelephonyManager
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       findViewById<TextView>(R.id.androidId).apply {
+           var string = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+           setText("ANDROID_ID:"+string)
+       }
+        val telephonyManager: TelephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * Dispatch onResume() to fragments.
-     */
-    override fun onPostResume() {
-        super.onPostResume()
+    fun getCallState(telephonyManager: TelephonyManager) : String {
+       return when (telephonyManager.callState) {
+            TelephonyManager.CALL_STATE_IDLE -> "CALL_STATE_IDLE"
+            TelephonyManager.CALL_STATE_RINGING -> "CALL_STATE_RINGING"
+            TelephonyManager.CALL_STATE_OFFHOOK -> "CALL_STATE_OFFHOOK"
+            else -> ""
+        }
     }
 
-    /**
-     * This is the fragment-orientated version of {@link #onResume()} that you
-     * can override to perform operations in the Activity at the same point
-     * where its fragments are resumed.  Be sure to always call through to
-     * the super-class.
-     */
-    override fun onResumeFragments() {
-        super.onResumeFragments()
+    private fun log(msg: String) {
+        Log.e("MainActivity", msg)
     }
+
 }
