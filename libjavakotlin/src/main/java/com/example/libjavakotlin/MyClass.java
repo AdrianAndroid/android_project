a5bcf8a96efca240c0f7e5c8da46db915fac71e2
zhaojian <flannery.zhao@kikatech.com>
2022-09-28 15:35:46 +0800

    某某某

--- libjavakotlin/src/main/java/com/example/libjavakotlin/MyClass.java
+++ libjavakotlin/src/main/java/com/example/libjavakotlin/MyClass.java
@@ -1,5 +1,7 @@
 package com.example.libjavakotlin;
 
+import javax.annotation.processing.Messager;
+
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
